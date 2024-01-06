d11cb7676e0ae01fb600181d73752880a63b1aa3
Danielle <dmaddo@uw.edu>
2024-01-05 17:46:54 -0800

    Added the patient class to hopefully make future scaling easier. Making changes to function to utalize the Patient class and hopefully help speed things up? In the middle of changes though so pretty messy currently

--- src/Functions.java
+++ src/Functions.java
@@ -1,25 +1,82 @@
 import java.util.*;
 import java.io.*;
-public class Functions {
+public class Functions extends Patient{
     private String name;
     //private String dateOfBirth;
     private boolean newPatient;
+    private boolean changed;
     private String patientFileName;
-    public Functions(String name, String dateOfBirth){
+    //private Set<String> medications;
+    public Functions(String name, String dateOfBirth) throws IOException {
+        super(name, dateOfBirth);
         //TO-DO: update to throw illegal argument if name = null or DOB is not in correct format : no longer need to address as it is handled in main
         this.name = name;
         // update dateOfBirth to no longer contain "/"
-        dateOfBirth = dateOfBirth.substring(0,2) + dateOfBirth.substring(3,5) + dateOfBirth.substring(6);//mmddyyyy
+        //dateOfBirth = dateOfBirth.substring(0,2) + dateOfBirth.substring(3,5) + dateOfBirth.substring(6);//mmddyyyy
 
-        this.patientFileName = name + "_" + dateOfBirth + ".txt";
+        this.patientFileName = getPatientFileName();
+        //medications = getMedications();
+        changed = false;
         //System.out.println(patientFileName);
         //check if there is an existing file for the patient
         //if not then newPatient = true
     }
-
+    private void prescribe(String newMedication) throws IOException {
+        if (!Patient.medications.contains(newMedication)){
+            this.changed = true;
+            Patient.medications.add(newMedication);
+            File file = getPatientFile();
+            if (Patient.medications.size() > 1){
+                Scanner patientFile = new Scanner(file);
+                Scanner database = new Scanner(new File ("Medication Database.txt"));
+                //search through database for medication
+                //TODO: 1/5/2024 I legit do not remeber what this is doing so go through it thoroughly
+                Set<String> databaseMed = new HashSet<>();
+                String interactions = "";
+                //todo: update to consider case
+                while (database.hasNextLine()){
+                    String line = database.nextLine();
+                    int breakpt = line.indexOf(":");
+                    databaseMed.add(line.substring(0,breakpt));
+                    if (databaseMed.contains(newMedication)) {
+                        interactions = line.substring(breakpt);
+                        break;
+                    }
+                }
+                //maybe add an if for if there are interactions to crosscheck against?
+                //make a list of all the medications contained in interactions
+                List<String> warning = new ArrayList<>();
+                while (patientFile.hasNextLine()){
+                    String medication = patientFile.nextLine();
+                    if (interactions.contains(medication)){
+                        warning.add(medication);
+                    }
+                }
+                if (warning.isEmpty()){
+                    //add the medication to the end of the patients file
+                    addMedication(newMedication, file);
+                } else{
+                    System.out.println("It appears that " + newMedication + " is not recommended to be taken with: ");
+                    for (String medication : warning){
+                        System.out.println(medication);
+                    }
+                    System.out.println("Knowing that there is a risk involved with using " + newMedication +
+                            " with current medications would you still like to prescribe it?");
+                    System.out.println("(Y)es or (N)o");
+                    Scanner console = new Scanner(System.in);
+                    String choice = console.next();
+                    if (choice.equalsIgnoreCase("y")){
+                        addMedication(newMedication, file);
+                    }
+                    //bad interactions warning with a list of each bad/faulty medication
+                    //check if they would like to add medication anyway
+                }
+            }
+        }
+    }
     //in this method used file reading and writing techniques from UW CSE 12x series
     //7-10-23: works almost as expected, some issues if developer is on the file where things get added to the same line instead of next line
-    public void prescribe(String newMedication) throws IOException {
+    /*public void prescribe(String newMedication) throws IOException {
         File file = new File(patientFileName);
         //7-8-2023: new patient branch works as expected/anticipated
         if (file.createNewFile()) { //if the patient did not already have a file, creates one
@@ -52,7 +109,7 @@ public class Functions {
                     warning.add(medication);
                 }
             }
-            if (warning.size() == 0){
+            if (warning.isEmpty()){
                 //add the medication to the end of the patients file
                 addMedication(newMedication, file);
             } else{
@@ -84,7 +141,7 @@ public class Functions {
             //if it does not conflict then add it to the end of the file
         }
 
-    }
+    }*/
 
     //add the medication to the end of the patients file
     //append to a file on 7-9-2023
@@ -106,7 +163,7 @@ public class Functions {
 
     //7/10/23: Preforms as expected for removing a medication from the patients file
     public void remove(String medication) throws IOException {
-        if (newPatient == false){
+        if (!newPatient){
             List<String> medications = new ArrayList<>();
             File file = new File(patientFileName);
             Scanner patientFile = new Scanner(file);
---
+++ src/Patient.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Patient { //information populated from patients file, used in functions
+    private String patientFileName;
+    private String name;
+    public static Set<String> medications;
+    private static String dateOfBirth;
+    public Patient(String name, String dob) throws IOException {
+        this.name = name;
+        dateOfBirth = dob.substring(0,2) + dob.substring(3,5) + dob.substring(6);//mmddyyyy
+        this.patientFileName = name + "_" + dateOfBirth + ".txt";
+        this.medications = new HashSet<>();
+        medications();
+    }
+    public String getDOB(){
+        return dateOfBirth;
+    }
+    public String getPatientFileName(){
+        return patientFileName;
+    }
+    public File getPatientFile() throws IOException {
+        File file = new File(patientFileName);
+        if (file.createNewFile()) { //if the patient did not already have a file, creates one
+            return file;
+        }
+        return file;
+    }
+    private void medications() throws IOException {
+        Scanner patientFile = new Scanner(this.getPatientFile());
+        while (patientFile.hasNextLine()){
+            String line = patientFile.nextLine();
+            medications.add(line);
+        }
+    }
+    public Set<String> getMedications(){
+        return medications;
+    }
+
+}
